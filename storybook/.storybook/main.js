d5bc6c43b1e27dcc49e2c76512238614c5533744
João Soares <jpcirqueira81@gmail.com>
2020-04-17 11:24:16 -0300

    teste button stories.

--- storybook/.storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app','@storybook/addon-actions', '@storybook/addon-links'],
-};
--- teste/src/button.stories.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-import { action } from '@storybook/addon-actions';
